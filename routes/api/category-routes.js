4acd164bb412f7fbd434e2b44ef961623e405cae
layne nigro <laynenigro@gmail.com>
2022-12-19 18:41:52 -0500

    added working rouets

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,26 +3,80 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/',async (req, res) => {
   // find all categories
+  try {
+const categoriesData = await Category.findAll({
+  include: [
+    {model:Product}
+  ]
+});
+res.json(categoriesData)
+} catch (error) {
+    console.log(error)
+  }
   // be sure to include its associated Products
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async(req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findByPk(req.params.id,{
+      include: [
+        {model:Product}
+      ]
+    });
+    res.json(categoryData)
+    } catch (error) {
+        console.log(error)
+      }
+  console.log("Getting by Id")
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    
+    console.log(req.body)
+    const newCategory = await Category.create(req.body)
+    res.json(newCategory)
+  } catch (error) {
+    console.log(error)
+
+  }
+  //category_name: "String"
+  
+
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id',async (req, res) => {
+  try {
+    
+    console.log(req.body)
+    const updatedCategory = await Category.update(req.body,{
+      where: {
+        id: req.params.id,
+      },
+    })
+    res.json(updatedCategory)
+  } catch (error) {
+    console.log(error)
+
+  }
   // update a category by its `id` value
+  console.log("Updating by Id")
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  const deletedCategory = await Category.destroy({
+    where: {
+      id: req.params.id,
+    },
+  });
+  res.json(deletedCategory);
+  console.log("Deleting by Id")
 });
 
 module.exports = router;
